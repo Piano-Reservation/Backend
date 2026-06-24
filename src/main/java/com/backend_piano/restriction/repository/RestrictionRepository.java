edce4a5067e0a80e406bae9d442e536534bfc0b7
유호찬 <yhc6563@gmail.com>
2026-06-24 15:54:18 +0900

    refactor: 제한 조회 쿼리 변경

--- src/main/java/com/backend_piano/restriction/repository/RestrictionRepository.java
+++ src/main/java/com/backend_piano/restriction/repository/RestrictionRepository.java
@@ -4,9 +4,14 @@ import com.backend_piano.restriction.model.Restriction;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestrictionRepository extends JpaRepository<Restriction, Long> {
 
-    Optional<Restriction> findFirstByStudentIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualOrderByEndDateDesc(
-            Long studentId, LocalDate startDate, LocalDate endDate);
+    @Query("SELECT r FROM Restriction r WHERE r.student.id = :studentId " +
+            "AND r.startDate <= :date AND r.endDate >= :date " +
+            "ORDER BY r.endDate DESC LIMIT 1")
+    Optional<Restriction> findCurrentByStudentId(@Param("studentId") Long studentId,
+                                                  @Param("date") LocalDate date);
 }
