bdc5f573f91a3818f599ccc790128c9290a774e6
JoonKyoLee <joonkyolee@gmail.com>
2026-06-27 21:06:25 +0900

    test: 스프링 테스트 환경 복구 및 예약 테스트 보강

--- src/test/java/com/backend_piano/BackendPianoApplicationTests.java
+++ src/test/java/com/backend_piano/BackendPianoApplicationTests.java
@@ -1,11 +1,13 @@
 package com.backend_piano;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BackendPianoApplicationTests {
 
     @Test
-    void 애플리케이션_테스트_환경이_구성된다() {
+    void contextLoads() {
     }
 
 }
